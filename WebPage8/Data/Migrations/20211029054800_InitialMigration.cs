6ff92d744593051c1d6e518d3cec24ca620f6f2e
jlivingstonsg <magnus.ivarsson@gmail.com>
2021-10-30 17:19:53 +0200

    Migration folder reset

---
+++ WebPage8/Data/Migrations/20211029054800_InitialMigration.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebPage8.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    BrandUrl = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    PostalCode = table.Column<string>(nullable: true),
+                    Street = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    Country = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Computers",
+                columns: table => new
+                {
+                    ComputerId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false),
+                    Processor = table.Column<string>(nullable: true),
+                    RAM = table.Column<string>(nullable: true),
+                    HardDisk = table.Column<string>(nullable: true),
+                    SystemType = table.Column<string>(nullable: true),
+                    ScreenSize = table.Column<string>(nullable: true),
+                    ImageUrl = table.Column<string>(nullable: true),
+                    CategoryId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Computers", x => x.ComputerId);
+                    table.ForeignKey(
+                        name: "FK_Computers_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderPlaced = table.Column<DateTime>(nullable: false),
+                    OrderFullfilled = table.Column<DateTime>(nullable: false),
+                    CustomerId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderId);
+                    table.ForeignKey(
+                        name: "FK_Orders_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "CustomerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ComputerOrders",
+                columns: table => new
+                {
+                    ComputerOrderId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Quantity = table.Column<int>(nullable: false),
+                    ShoppingCartId = table.Column<string>(nullable: true),
+                    ComputerId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ComputerOrders", x => x.ComputerOrderId);
+                    table.ForeignKey(
+                        name: "FK_ComputerOrders_Computers_ComputerId",
+                        column: x => x.ComputerId,
+                        principalTable: "Computers",
+                        principalColumn: "ComputerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reviews",
+                columns: table => new
+                {
+                    ReviewId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Text = table.Column<string>(nullable: true),
+                    Rating = table.Column<int>(nullable: false),
+                    ComputerId = table.Column<int>(nullable: false),
+                    CustomerId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviews", x => x.ReviewId);
+                    table.ForeignKey(
+                        name: "FK_Reviews_Computers_ComputerId",
+                        column: x => x.ComputerId,
+                        principalTable: "Computers",
+                        principalColumn: "ComputerId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Reviews_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "CustomerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "BrandUrl", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "images/Brand/acer.jpg", "Acer incorporated is now one of the biggest brands in computer hardware and electronics. They're also one of the biggest laptop brands known for their cheap and affordable laptop computers.", "Acer" },
+                    { 2, "images/Brand/samsung.jpg", "Samsung is one of the best laptop manufacturers right now, and over the last few years, it has released some of the best Windows laptops available. Featuring powerful specs, great battery life, and premium designs, Samsung devices are consistently cutting edge and high quality", "Samsung" },
+                    { 3, "images/Brand/Lenovo.jfif", "Lenovo is truly an excellent brand to get for your laptop needs! They offer proven and tested reliability for their laptops, and also an amazing value at every price for every user.", "Lenovo" },
+                    { 4, "images/Brand/Hp.png", "Through it all, HP has earned a reputation for reliable laptops with very competent customer service. Today, HP regularly goes head-to-head with some of the best laptop manufacturers in the world. Customer support options place HP in the top five of all manufacturers.", "Hp" },
+                    { 5, "images/Brand/Hp.png", "ASUS is a good and reputable brand, the brand is well-known for its optimized performance with the lowest computer specifications. In the fiercely competitive market for computing and hand-held devices, it is becoming extremely difficult for device and gadget manufacturers to break the clutter and stand out from the competition.", "Asus" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "CustomerId", "City", "Country", "Email", "FirstName", "LastName", "PhoneNumber", "PostalCode", "Street" },
+                values: new object[,]
+                {
+                    { 1, "Goteborg", "Sweden", "za@a.gmail.com", "Wei", "C", "+12345678919", "888", "Möland" },
+                    { 2, "Skåne", "Sweden", "ya@a.gmail.com", "Magnus", "I", "+12345678919", "", "aaa" },
+                    { 3, "Skövde", "Sweden", "xa@a.gmail.com", "Muzdalifa", "I", "+99345678919", "123", "Frostaliden" },
+                    { 4, "Uppsala", "Sweden", "aa@a.gmail.com", "Seong", "Gi-hun", "+33345678919", "123", "bbb" },
+                    { 5, "Skövde", "Sweden", "ba@a.gmail.com", "Kang ", "Sae-Byeok", "+12345678919", "345", "Hjo" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Computers",
+                columns: new[] { "ComputerId", "CategoryId", "HardDisk", "ImageUrl", "Name", "Price", "Processor", "RAM", "ScreenSize", "SystemType" },
+                values: new object[,]
+                {
+                    { 1, 1, "128GB", "../images/pro2.png", "A", 4500m, "Intel(R) Core(TM) i7-8700K CPU @ 3.7GHz3.70", "32GB", "4'", "64-bit operating system, x64-based processor" },
+                    { 2, 1, "512 GB SSD", "../images/pro2.png", "B", 5990m, "Intel Pentium Silver N6000-processor", "8GB", "No pen and touch input is available for this display", "64-bit operating system, x64-based processor" },
+                    { 3, 1, "64GB", "../images/pro2.png", "C", 2990m, "Intel® Celeron® N4020 processor", "4GB", "14' Full HD - skärm(1920 x 1080)", "64-bit operating system, x64-based processor" },
+                    { 4, 1, "64GB", "../images/pro2.png", "C", 4490m, "AMD Athlon Silver 3050C processor", "4GB", "14' Full HD - pekskärm", "64-bit operating system, x64-based processor" },
+                    { 5, 1, "512 GB SSD", "../images/pro2.png", "C", 11990m, "Intel® Core™ i5-10300H processor", "16 GB", "120 Hz 17,3' Full HD - skärm", "64-bit operating system, x64-based processor" },
+                    { 6, 1, "512 GB", "../images/pro2.png", "C", 6990m, "AMD Ryzen 5 4500U processor", "16 GB", "14' Full HD - skärm(IPS - panel)", "64-bit operating system, x64-based processor" },
+                    { 7, 2, "", "../images/pro2.png", "D", 2000m, "Intel(R) Core(TM) i7-8700K CPU @ 3.7GHz3.70", "32GB", "No pen and touch input is available for this display", "64-bit operating system, x64-based processor" },
+                    { 8, 3, "", "../images/pro2.png", "E", 2000m, "Intel(R) Core(TM) i7-8700K CPU @ 3.7GHz3.70", "32GB", "No pen and touch input is available for this display", "64-bit operating system, x64-based processor" },
+                    { 9, 3, "", "../images/pro2.png", "F", 2000m, "Intel(R) Core(TM) i7-8700K CPU @ 3.7GHz3.70", "32GB", "No pen and touch input is available for this display", "64-bit operating system, x64-based processor" },
+                    { 10, 4, "", "../images/pro2.png", "G", 2000m, "Intel(R) Core(TM) i7-8700K CPU @ 3.7GHz3.70", "32GB", "No pen and touch input is available for this display", "64-bit operating system, x64-based processor" },
+                    { 11, 4, "", "../images/pro2.png", "H", 2000m, "Intel(R) Core(TM) i7-8700K CPU @ 3.7GHz3.70", "32GB", "No pen and touch input is available for this display", "64-bit operating system, x64-based processor" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Reviews",
+                columns: new[] { "ReviewId", "ComputerId", "CustomerId", "Rating", "Text" },
+                values: new object[,]
+                {
+                    { 1, 1, 1, 5, "Bla bla bla" },
+                    { 5, 1, 1, 1, "Bla bla bla" },
+                    { 2, 2, 2, 3, "Bla bla bla" },
+                    { 6, 2, 2, 1, "Bla bla bla" },
+                    { 3, 3, 3, 5, "Bla bla bla" },
+                    { 7, 3, 3, 4, "Bla bla bla" },
+                    { 4, 4, 4, 3, "Bla bla bla" },
+                    { 8, 4, 4, 2, "Bla bla bla" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ComputerOrders_ComputerId",
+                table: "ComputerOrders",
+                column: "ComputerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Computers_CategoryId",
+                table: "Computers",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_CustomerId",
+                table: "Orders",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_ComputerId",
+                table: "Reviews",
+                column: "ComputerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_CustomerId",
+                table: "Reviews",
+                column: "CustomerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ComputerOrders");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Reviews");
+
+            migrationBuilder.DropTable(
+                name: "Computers");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
