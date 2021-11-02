d37b72265ce8a04be68ec5a210063634e087b8d3
Muzdalifa <a.muzdalifa27@gmail.com>
2021-11-02 11:36:59 +0100

    checkpoint

--- WebPage8/Controllers/ReviewController.cs
+++ WebPage8/Controllers/ReviewController.cs
@@ -36,18 +36,7 @@ namespace WebPage8.Controllers
             _applicationDbContext.Reviews.Add(review);
             _applicationDbContext.SaveChanges();
 
-            Computer  computer= _computerService.FindBy(id);
-
-            ComputerViewModel compDetail = new ComputerViewModel
-            {
-                Computer = computer
-            };
-
-            //return RedirectToActionResult("Detail", "Product", compDetail);
-
-            return RedirectToAction("..//Detail", compDetail);
-
-            //return View("../Product/Detail", compDetail);
+            return RedirectToAction("Detail", "Product", new { computerId = id });
         }
     }
 }
